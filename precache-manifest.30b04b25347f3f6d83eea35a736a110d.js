8decd8413a286fe00dd3b27ac5a4a1f570f73a27
thomas utsey <tomutsey@yahoo.com>
2020-02-29 12:02:36 -0500

    Updates

---
+++ precache-manifest.30b04b25347f3f6d83eea35a736a110d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427674440fc78cb858842828ac5b8014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91f562af269cd36206c",
+    "url": "/static/css/main.a9f19f20.chunk.css"
+  },
+  {
+    "revision": "24d00c40ad8c3f49fa1f",
+    "url": "/static/js/2.25c6bcd2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.25c6bcd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b91f562af269cd36206c",
+    "url": "/static/js/main.42053fde.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  }
+]);
--- src/components/contact/contact.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const Contact = () =>{
-  return(<div>contact me</div>)
-}
-export default Contact
