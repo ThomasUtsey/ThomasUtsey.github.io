063a8aedd17ef0a889231ab2a78a1159d1b2e377
Thomas Utsey <tom@edgegluer.com>
2020-03-15 21:40:14 -0400

    add hamburger menu for mobile display

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,60 +4,85 @@ import { NavLink } from "react-router-dom";
 import "./nav.css";
 
 class Nav extends React.Component {
-  constructor(){
-    super()
-  this.state = {
-    toggle:'close'
+  constructor() {
+    super();
+    this.state = {
+      toggle: "close"
+    };
   }
-}
   onClickHandler = () => {
-   
     return this.state.toggle === "close"
-      ? this.setState({toggle:"nav-child open"})
-      : this.setState({toggle:"close"})
+      ? this.setState({ toggle: "nav-child open" })
+      : this.setState({ toggle: "close" });
   };
- render(){
-   return(
-    <div className="nav">
-      <i onClick={this.onClickHandler} className="fas fa-bars burger"></i>
+  render() {
+    return (
+      <div className="nav">
+        <div onClick={this.onClickHandler} className="burger">
+          <a id="nav-toggle" href="#">
+            <span></span>
+          </a>
+        </div>
 
-      <div className={this.state.toggle}>
-        {" "}
-        <NavLink style={{"color":"black"}} to="/" onClick={()=>this.setState({toggle:"close"})}>
-          Home{" "}
-        </NavLink>
+        <div className={this.state.toggle}>
+          {" "}
+          <NavLink
+            style={{ color: "black" }}
+            to="/"
+            onClick={() => this.setState({ toggle: "close" })}
+          >
+            Home{" "}
+          </NavLink>
+        </div>
+        <div className={this.state.toggle}>
+          {" "}
+          <NavLink
+            style={{ color: "black" }}
+            to="/projects"
+            onClick={() => this.setState({ toggle: "close" })}
+          >
+            Projects{" "}
+          </NavLink>
+        </div>
+        <div className={this.state.toggle}>
+          <NavLink
+            style={{ color: "black" }}
+            to="/education"
+            onClick={() => this.setState({ toggle: "close" })}
+          >
+            Education{" "}
+          </NavLink>
+        </div>
+        <div className={this.state.toggle}>
+          <NavLink
+            style={{ color: "black" }}
+            to="/experience"
+            onClick={() => this.setState({ toggle: "close" })}
+          >
+            Experience{" "}
+          </NavLink>
+        </div>
+        <div className={this.state.toggle}>
+          {" "}
+          <NavLink
+            style={{ color: "black" }}
+            to="/goals"
+            onClick={() => this.setState({ toggle: "close" })}
+          >
+            Blog
+          </NavLink>
+        </div>
+        <div className={this.state.toggle}>
+          <NavLink
+            style={{ color: "black" }}
+            to="/contact"
+            onClick={() => this.setState({ toggle: "close" })}
+          >
+            Contact Me{" "}
+          </NavLink>
+        </div>
       </div>
-      <div className={this.state.toggle}>
-        {" "}
-        <NavLink style={{"color":"black"}} to="/projects" onClick={()=>this.setState({toggle:"close"})}>
-          Projects{" "}
-        </NavLink>
-      </div>
-      <div className={this.state.toggle}>
-        <NavLink style={{"color":"black"}} to="/education" onClick={()=>this.setState({toggle:"close"})}>
-          Education{" "}
-        </NavLink>
-      </div>
-      <div className={this.state.toggle}>
-        <NavLink style={{"color":"black"}} to="/experience" onClick={()=>this.setState({toggle:"close"})}>
-          Experience{" "}
-        </NavLink>
-      </div>
-      <div className={this.state.toggle}>
-        {" "}
-        <NavLink style={{"color":"black"}} to="/goals" onClick={()=>this.setState({toggle:"close"})}>
-          Blog
-        </NavLink>
-      </div>
-      <div className={this.state.toggle}>
-        <NavLink style={{"color":"black"}} to="/contact" onClick={()=>this.setState({toggle:"close"})}>
-          Contact Me{" "}
-        </NavLink>
-      </div>
-    </div>
-   )
- }
-  
-
+    );
+  }
 }
 export default Nav;
