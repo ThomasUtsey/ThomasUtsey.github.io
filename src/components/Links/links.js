decbe9d3394724d750ecdb0ebfe70d6e13c828f3
Thomas Utsey <tom@edgegluer.com>
2020-04-02 22:45:57 -0400

    Add text to link icons to clarify Resume icon

--- src/components/Links/links.js
+++ src/components/Links/links.js
@@ -10,12 +10,15 @@ const Links = () => {
     <div className="link-container">
       <a target="_blank" href="https://github.com/ThomasUtsey">
         <img src={github} alt="Github icon" />
+        Github
       </a>
       <a target="_blank" href="https://www.linkedin.com/feed/">
         <img src={linkedin} alt="Linked In icon" />
+        Linkedin
       </a>
       <a target="_blank" href="https://resume.creddle.io/resume/66v1k2i2kf6">
         <img src={resume} alt="Resume Icon" />
+        Resume
       </a>
     </div>
   );
