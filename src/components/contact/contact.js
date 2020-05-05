16eb5ffb1c96d77fbf51336622acc8e52fad5c98
thomas utsey <tomutsey@yahoo.com>
2020-05-05 18:08:32 -0400

    Add captcha

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import emailjs from "emailjs-com";
 import "./contact.css";
+import Recaptcha from "react-recaptcha";
 
 class Contact extends React.Component {
   constructor(props) {
@@ -8,47 +9,61 @@ class Contact extends React.Component {
     this.state = {
       user_name: "",
       user_email: "",
-      message: ""
+      message: "",
+      isVerified: false,
     };
   }
-
-  sendEmail = e => {
-    e.preventDefault();
-    console.log(e);
-
-    const data = {
-      template_params: {
-        to_name: "Thomas",
-        email: this.state.user_email,
-        from_name: this.state.user_name,
-        message_html: this.state.message
-      }
-    };
-
-    emailjs
-      .send(
-        "gmail",
-        "template_4XZrIpKt",
-        data.template_params,
-        "user_WbeUYLO8TMaKH3hBSVqbe"
-      )
-      .then(
-        function(response) {
-          console.log("SUCCESS!", response.status, response.text);
-        },
-        function(error) {
-          console.log("FAILED...", error);
-        }
-      );
-    this.setState({
-      user_name: "",
-      user_email: "",
-      message: ""
-    });
+  recaptchaLoaded = (e) => {
+    console.log("loaded");
   };
-  handleChange = event => {
+  verify = (res) => {
+    if (res) {
+      this.setState({ isVerified: true });
+    }
+  };
+  sendEmail = (e) => {
+    e.preventDefault();
+
+    if (this.state.isVerified) {
+      console.log(e);
+
+      const data = {
+        template_params: {
+          to_name: "Thomas",
+          email: this.state.user_email,
+          from_name: this.state.user_name,
+          message_html: this.state.message,
+        },
+      };
+
+      emailjs
+        .send(
+          "gmail",
+          "template_4XZrIpKt",
+          data.template_params,
+          "user_WbeUYLO8TMaKH3hBSVqbe"
+        )
+        .then(
+          function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          function (error) {
+            console.log("FAILED...", error);
+          }
+        );
+      this.setState({
+        user_name: "",
+        user_email: "",
+        message: "",
+      });
+      alert("The email has been sent");
+    } else {
+      alert("You must click captcha if you are human");
+    }
+  };
+  handleChange = (event) => {
     const {
-      target: { name, value }
+      target: { name, value },
     } = event;
     this.setState({ [name]: value, event: event });
   };
@@ -83,6 +98,12 @@ class Contact extends React.Component {
           onChange={this.handleChange}
         />
         <input className="button" type="submit" value="Send" />
+        <Recaptcha
+          sitekey="6LdQDvMUAAAAAAuK5i-pNwLT0axKIKL6_Do7Jwfr"
+          render="explicit"
+          onloadCallback={this.recaptchaLoaded}
+          verifyCallback={this.verify}
+        />
       </form>
     );
   }
