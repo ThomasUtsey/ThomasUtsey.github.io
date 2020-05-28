ad0874d370b30e53e7a2a7d502244bcc74b02bc1
thomas utsey <tomutsey@yahoo.com>
2020-05-28 17:35:16 -0400

    Improve and edit media queries

--- src/components/profile-card/profileCard.js
+++ src/components/profile-card/profileCard.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import './profilecard.css'
+import React from "react";
+import "./profilecard.css";
 
-const ProfileCard = ({data}) =>{
- 
-return(
-    <div className='profile-card'>
-     <div className = 'name-container'>
-     {/* <img src={data.picture} alt="pic of me"/> */}
-    <div><h1>{data.name}</h1>
-    <h2>{data.label}</h2>
+const ProfileCard = ({ data }) => {
+  return (
+    <div className="profile-card">
+      <div className="name-container">
+        {/* <img src={data.picture} alt="pic of me"/> */}
+        <div>
+          <h1>{data.name}</h1>
+          <h2>{data.label}</h2>
+        </div>
+      </div>
+      <div className="profile-card-mid">
+        <div className="headline">
+          <h2>"{data.headline}"</h2>
+        </div>
+      </div>
     </div>
-    </div>
-    <div className='profile-card-mid'>
-    
-   
-    <div className='headline'>
-    <h2>"{data.headline}"</h2>
-    </div>
-    </div>
-    </div>
-)
-}
-export default ProfileCard
+  );
+};
+export default ProfileCard;
