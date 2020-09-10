24f9e274c20f742364f97d45334aada4b2402772
thomas utsey <tomutsey@yahoo.com>
2020-09-10 18:20:24 -0400

    add cert badges

--- src/components/skills/skillscard.js
+++ src/components/skills/skillscard.js
@@ -6,16 +6,6 @@ const SkillCard = ({ skills }) => {
   return (
     <div className="skill-container">
       <div className="name">{skills.name}</div>
-      <div className="bar">
-        <div className="bar-name"> Profficiency: </div>
-              <div id="bar-indicator">{skills.rating}
-              .bar-segemnt
-              
-              </div>
-      </div>
-      <div className="yoe">
-        Experience:{skills.yearsOfExperience * 12} months
-      </div>
     </div>
   );
 };
